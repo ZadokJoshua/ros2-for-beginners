5c436cdcfe91cc4bf2771cebb353a04e0db28751
ZadokJoshua <ayegbazadok@outlook.com>
2024-11-29 02:04:57 +0100

    Initial update

--- build/my_py_pkg/colcon_command_prefix_setup_py.sh
+++ build/my_py_pkg/colcon_command_prefix_setup_py.sh
@@ -1,1 +1,2 @@
 # generated from colcon_core/shell/template/command_prefix.sh.em
+. "/home/zadok/ros2_ws/src/install/my_robot_interfaces/share/my_robot_interfaces/package.sh"
--- my_py_pkg/my_py_pkg/hw_status_publisher.py
+++ my_py_pkg/my_py_pkg/hw_status_publisher.py
@@ -5,7 +5,7 @@ from rclpy.node import Node
 from my_robot_interfaces.msg import HardwareStatus
 
 class HardwareStatusPublisherNode(Node):
-    def _init_(self):
+    def __init__(self):
         super().__init__("hardware_status_publisher")
         self.hw_status_publisher_ = self.create_publisher(HardwareStatus, "hardware_status", 10)
         self.timer = self.create_timer(1.0, self.publish_hw_status)
