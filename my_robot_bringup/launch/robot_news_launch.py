1863f1d45e6d6229fd51c8b2d163b95a8874cdd3
ZadokJoshua <ayegbazadok@outlook.com>
2024-12-06 10:07:32 +0100

    Fix: Improve generate_launch_description

--- my_robot_bringup/launch/robot_news_launch.py
+++ my_robot_bringup/launch/robot_news_launch.py
@@ -5,23 +5,22 @@ def generate_launch_description():
     ld =  LaunchDescription()
     
     remap_robot_news_topic = ("robot_news","minna_robot_news")
+    robot_names = ["Chapi", "Atlas", "Swiss_Mile", "Mentee", "Zadok"]
+    robot_nodes = []
     
-    robots = ["Chapi", "Atlas", "Swiss_Mile", "Mentee", "Zadok"]
-    
-    for robot in robots:
-        robot_news_station_node = Node(
-            package="my_py_pkg",
-            executable="robot_news_station",
-            name=f"{robot}_robot_news_station",
-            remappings=[
-                remap_robot_news_topic
-            ],
-            parameters=[
-                {"robot_name": robot}
-            ]
-        )
-        
-        ld.add_action(robot_news_station_node)
+    for robot in robot_names:
+        robot_nodes.append(
+            Node(
+                package="my_py_pkg",
+                executable="robot_news_station",
+                name=f"{robot}_robot_news_station",
+                remappings=[
+                    remap_robot_news_topic
+                ],
+                parameters=[
+                    {"robot_name": robot}
+                ]
+            ))
         
         
     smartphone_node = Node(
@@ -33,6 +32,8 @@ def generate_launch_description():
         ]
     )
     
+    for node in robot_nodes:
+        ld.add_action(node)
     ld.add_action(smartphone_node)
     
     return ld 
